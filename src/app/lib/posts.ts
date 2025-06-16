0738daadd8a323c258e444fd41e107d5f28f1ef4
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2025-06-16 23:00:42 +0900

    main

---
+++ src/app/lib/posts.ts
@@ -0,0 +1,8 @@
+export function getPosts() {
+  return [
+    { id: 1, title: "글1", summary: "첫번째 등록 글" },
+    { id: 2, title: "글2", summary: "두번째 등록 글" },
+    { id: 3, title: "글3", summary: "세번째 등록 글" },
+    { id: 4, title: "글4", summary: "네번째 등록 글" },
+  ];
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import PostCard from "@/components/PostCards";
+
 export default function Home() {
+  const posts = [
+    { id: 1, title: "글1", summary: "첫번째 등록 글" },
+    { id: 2, title: "글2", summary: "두번째 등록 글" },
+    { id: 3, title: "글3", summary: "세번째 등록 글" },
+    { id: 4, title: "글4", summary: "네번째 등록 글" },
+  ];
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+    <main className="min-h-screen bg-[#fefae0] p-8 flex flex-col">
+      <div className="flex flex-1">
+        <section className="flex-1">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {posts.map((post) => (
+              <PostCard
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                summary={post.summary}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
