8032989857d608d23eb9ae7ecf7a0f78c43d7996
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2025-07-30 16:10:48 +0900

    post

--- src/app/lib/posts.ts
+++ src/app/lib/posts.ts
@@ -2,7 +2,10 @@ import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
 import { remark } from "remark";
-import html from "remark-html";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeHighlight from "rehype-highlight";
+import rehypeStringify from "rehype-stringify";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -18,11 +21,11 @@ export type Post = {
 
 export async function getPosts(): Promise<Omit<Post, "content">[]> {
   const fileNames = await fs.readdir(postsDirectory);
-  const filtered = fileNames.filter((name) => /^\d+\.md$/.test(name));
+  const filtered = fileNames.filter((name) => /^\d+\.mdx$/.test(name));
 
   const posts = await Promise.all(
     filtered.map(async (fileName) => {
-      const id = parseInt(fileName.replace(/\.md$/, ""), 10);
+      const id = parseInt(fileName.replace(/\.mdx$/, ""), 10);
       const fullPath = path.join(postsDirectory, fileName);
       const fileContents = await fs.readFile(fullPath, "utf8");
       const { data } = matter(fileContents);
@@ -42,12 +45,18 @@ export async function getPosts(): Promise<Omit<Post, "content">[]> {
 }
 
 export async function getPostById(id: number): Promise<Post | null> {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fullPath = path.join(postsDirectory, `${id}.mdx`);
   try {
     const fileContents = await fs.readFile(fullPath, "utf8");
     const { data, content } = matter(fileContents);
 
-    const processedContent = await remark().use(html).process(content);
+    const processedContent = await remark()
+      .use(remarkGfm) // GitHub-flavored markdown
+      .use(remarkRehype) // remark -> rehype
+      .use(rehypeHighlight) // 코드 하이라이트 적용
+      .use(rehypeStringify) // rehype -> HTML 문자열
+      .process(content);
+
     const contentHtml = processedContent.toString();
 
     return {
